c389f5aff13fe9bb923af4de0ab01a1ed8bca3c5
Michael <michaelmccormack49@gmail.com>
2016-01-21 07:55:28 -0500

    Updated get_special_paths.py

--- get_special_paths.py
+++ get_special_paths.py
@@ -1,30 +1,12 @@
 
 import sys
-#import re
-#import os
-#import shutil
-# import commands
+import re
+import os
+import shutil
+import commands
 
-# def get_special_paths(argv[1]):
-#    path = os.path.abspath(argv[1])
-#    print path
+#def get_special_paths(argv[1]):
+path = os.path.abspath(sys.argv[1])
+print path
     
-arg = sys.argv[1]
-arg2 = sys.argv[2]
 
-def tester(num):
-    print int(num) + 2
-
-tester(arg)
-
-tester(arg2)
-
-print "this is great"
-
-print "I think I got it"
-
-print "Now I must ask"
-
-xa = input() 
-
-print xa
