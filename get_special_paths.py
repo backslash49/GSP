962cb486d9809d1aff2a6eb58bcaea15eec75dad
Michael <michaelmccormack49@gmail.com>
2016-01-21 07:56:49 -0500

    Updated get_special_paths.py

--- get_special_paths.py
+++ get_special_paths.py
@@ -5,8 +5,17 @@ import os
 import shutil
 import commands
 
-#def get_special_paths(argv[1]):
-path = os.path.abspath(sys.argv[1])
-print path
-    
+#creates list of files in parent directory
+files = os.listdir('./')
+
+#convert list of files to string
+filesstring = str(files)
+
+#locate 'special files' with [asd__asdfasd_asdf.as] as the formatting
+specialfiles = re.findall('\w*__\w*__.\w*', filesstring)
+
+#invoke for loop to return absolute path for each 'special' file
+for files in specialfiles:
+    paths = os.path.abspath(files)
+    print paths
 
