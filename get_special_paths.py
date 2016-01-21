060631e12a72c3b2723fae4e8b8fb1809ed5087b
Lauren Camar <lauren@Laurens-MBP.home>
2016-01-20 21:01:52 -0500

    Second commit

---
+++ get_special_paths.py
@@ -0,0 +1,30 @@
+
+import sys
+#import re
+#import os
+#import shutil
+# import commands
+
+# def get_special_paths(argv[1]):
+#    path = os.path.abspath(argv[1])
+#    print path
+    
+arg = sys.argv[1]
+arg2 = sys.argv[2]
+
+def tester(num):
+    print int(num) + 2
+
+tester(arg)
+
+tester(arg2)
+
+print "this is great"
+
+print "I think I got it"
+
+print "Now I must ask"
+
+xa = input() 
+
+print xa
