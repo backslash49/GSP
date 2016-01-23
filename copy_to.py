613920ea4380d425553384f30b8415fd305f625c
Michael <michaelmccormack49@gmail.com>
2016-01-23 09:20:38 -0500

    GSP is now fully functioning

--- copy_to.py
+++
@@ -1,7 +0,0 @@
-import shutil
-
-def copy_to(files, directory):
-    shutil.copy(files, directory)
-
-
-
--- gsp.py
+++ gsp.py
@@ -9,8 +9,9 @@ def get_special_paths(dir):
 #this function accepts a directory as an argumentand returns the absolute paths of any
 #files formated like this: asdf__asdfa__.asd
 
-
-    #creates list of files in parent directory
+    result = []
+    
+    #creates list of files in named directory
     files = os.listdir(dir)
 
     #converts list of files to string
@@ -21,18 +22,44 @@ def get_special_paths(dir):
 
     #invokes for loop to print absolute path for each 'special' file
     for files in specialfiles:
-        paths = os.path.abspath(files)
-        return paths
-
-def copy_to(files, directory):
-    shutil.copy(files, directory)
+        result.append(os.path.abspath(os.path.join(files)))
+    return result
 
 
+#    shutil.copy(files, '/Users/lauren/Downloads/google-python-exercises')
+
+    
 #for files in paths:
 #    copy_to(files, '/Users/Lauren')
 
-def main(directoryname):
-    get_special_paths(directoryname)
 
+def copy_to(specialfilepaths, directory):
+    if not os.path.exists(directory): #creates directory if it doesnt exist
+        os.mkdir(directory)
+    for path in specialfilepaths:   #runs for loop on files in specialfilepaths       
+        shutil.copy(path, os.path.join(directory)) #copies to directory
+
+def main():
+    args = sys.argv[1:]
+    if not args:
+        print "usage: [--todir dir][--tozip zipfile] dir [dir ...]";
+        sys.exit(1)
+    todir = '' #tudir = where its going
+    if args[0] == '--todir':
+        todir = args[1] #defining tudir from cmd line
+        del args[0:2] 
+    if len(args) == 0:
+        print "error: must specify one or more directories"
+        sys.exit(1)
+
+    paths = [] #blank list to be populated with with special filenames
+    dirname = sys.argv[3] #input location for special files
+    paths.extend(get_special_paths(dirname)) #creating list of special files in 
+    if todir:
+        copy_to(paths, todir)
     
+    
+#    initiates main() function on cmd line, if present in function
+if __name__ == "__main__":
+  main()
 
